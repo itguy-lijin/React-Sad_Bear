7a52f5aa346df921fea95abe088f20183c4e72eb
nicolaefordon <fn.alin.business@gmail.com>
2022-05-29 12:19:30 -0400

    merge main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Homepage from './pages/home/home'
+import SadBear from './pages/sadBear/sadBear'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Homepage} />
+        <Route path="/sadBear" exact component={SadBear} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
